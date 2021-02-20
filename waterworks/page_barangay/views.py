a7f47360523dc795030a09dfc8d0044eda79f7e2
Ayan Sumania <DJSUMANIA@Ayans-MacBook-Pro.local>
2021-02-20 23:54:05 +0800

    barangay ajax modified unfinished

--- waterworks/page_barangay/views.py
+++ waterworks/page_barangay/views.py
@@ -15,18 +15,20 @@ from waterworks.models import Barangay
 
 class Waterworks_Barangay_AJAXView(View):
     queryset = Barangay.objects.all()
-    template_name = 'waterworks/tables/barangay.html'
+    template_name = 'waterworks/tables/table_barangay.html'
     def get(self, request):
         data = dict()
         try:
             search = self.request.GET.get('search')
-            filter = self.request.GET.get('filter')
+            start = self.request.GET.get('start')
+            end = self.request.GET.get('end')
         except KeyError:
             search = None
-            filter = None
-        if search or filter:
+            start = None
+            end = None
+        if search or start or end:
             data['form_is_valid'] = True
             data['counter'] = self.queryset.filter(name__icontains = search).count()
-            profile = self.queryset.filter(name__icontains = search).order_by('surname','firstname')[:int(filter)]
-            data['table'] = render_to_string(self.template,{'profile':profile})
+            barangay = self.queryset.filter(name__icontains = search).order_by('name')[int(start):int(end)]
+            data['table'] = render_to_string(self.template,{'barangay':barangay})
         return JsonResponse(data)
